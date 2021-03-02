27ae34dd969517f4e0e6d806e59c60bc72512c97
George <gchriscarter@gmail.com>
2021-03-01 22:38:48 -0500

    add # to currency, tax, total and discount model methods

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -17,14 +17,14 @@ class Api::ProductsController < ApplicationController
       image_url: params[:image_url],
       description: params[:description],
     )
-    @product.save
+    #@product.save
     render "show.json.jb"
   end
 
   def update
     @product = Product.find_by(id: params[:id])
-    @product.name = params[:name] || @product.name
-    @product.price = params[:price] || @product.price
+    @product.name = params[:name]
+    @product.price = params[:price]
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
     @product.save

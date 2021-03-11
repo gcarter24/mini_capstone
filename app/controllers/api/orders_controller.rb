3b8a52b6d33e24d9c7cf5ae831282e7dd4c3c6dc
George <gchriscarter@gmail.com>
2021-03-10 19:47:35 -0500

    make orders more dynamic and to calculate accurate totals

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -9,13 +9,17 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
+    product = Product.find_by(id: params[:product_id])
+    calc_subtotal = params[:quantity].to_i * product.price
+    calc_tax = calc_subtotal * 0.09
+    calc_total = calc_subtotal + calc_tax
     @order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: Product.find_by(id: params[:product_id]).price,
-      tax: Product.find_by(id: params[:product_id]).tax,
-      total: Product.find_by(id: params[:product_id]).total,
+      subtotal: calc_subtotal,
+      tax: calc_tax,
+      total: calc_total,
     )
     @order.save
     render "show.json.jb"
